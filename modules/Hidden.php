471fb43c70408fba28b6663a9ed69f5eda63ed75
Fabian Pirklbauer <fabian@pirklbauer.cc>
2014-12-27 17:43:08 +0100

    added hidden field check

--- modules/Hidden.php
+++ modules/Hidden.php
@@ -3,12 +3,19 @@ namespace Sbnc\Modules;
 
 class Hidden extends Module implements ModuleInterface {
 
+    private $errors = [
+        'error'     => 'Spam! %field% is not empty.'
+    ];
+
     protected function init() {
         $this->master['fields']['check'] = null;
     }
 
     public function check() {
-
+        if (!isset($this->master['request']['check']) ||  strlen(trim($this->master['request']['check'])) != 0) {
+            $err = str_replace('%field%', 'check', $this->errors['error']);
+            array_push($this->master['errors'], $err);
+        }
     }
 
 }
