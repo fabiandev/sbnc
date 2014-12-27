dea8043183e2604d8783cb54ce4cf8f7d46e38be
Fabian Pirklbauer <fabian@pirklbauer.cc>
2014-12-27 12:00:06 +0100

    added validations

--- modules/Validate.php
+++ modules/Validate.php
@@ -15,17 +15,64 @@ class Validate extends Module implements ModuleInterface {
      *
      * @var array
      */
+    private $errors = [
+        'email'     => 'Email is not valid.',
+        'url'       => 'URL is not valid.',
+        'required'  => '%field% is required'
+    ];
+
     private $options = [
-        'email' => ['email', 'mail'],
-        'url'   => ['url', 'link', 'web']
+        'email'    => ['email', 'mail'],
+        'url'      => ['url', 'link', 'web'],
+        'required' => ['email', 'name', 'message']
     ];
 
     protected function init() {
 
     }
 
-    public function check() {
+    protected function check() {
+        foreach ($this->options as $key => $value) {
+            foreach ($value as $name) {
+                if (isset($this->master['request'][$name])) {
+                    $func = 'validate_' . $name;
+                    $this->$func($this->master['request'][$name], $name);
+                }
+            }
+        }
+    }
 
+    protected function validate_required($value, $name) {
+        if (strlen(trim($value)) == 0) {
+            $err = str_replace('%field%', $name, $this->errors['required']);
+            array_push($this->master['errors'], $err);
+            return false;
+        }
+        return true;
+    }
+
+    protected function validate_email($value, $name) {
+        if (filter_var($value, FILTER_VALIDATE_EMAIL) === false) {
+            array_push($this->master['errors'], $this->errors['email']);
+            return false;
+        }
+        return true;
+    }
+
+    protected function validate_ip($value, $name) {
+        if (filter_var($value, FILTER_VALIDATE_IP) === false) {
+            array_push($this->master['errors'], $this->errors['ip']);
+            return false;
+        }
+        return true;
+    }
+
+    protected function validate_url($value, $name) {
+        if (filter_var($value, FILTER_VALIDATE_URL) === false) {
+            array_push($this->master['errors'], $this->errors['url']);
+            return false;
+        }
+        return true;
     }
 
 }
